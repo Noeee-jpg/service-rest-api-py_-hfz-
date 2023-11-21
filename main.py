8719a2999299fd2e03542cdea2359fffd40e5dac
noeee-jpg <hnoer4646@gmail.com>
2023-11-21 20:42:06 +0700

    chore :update main.py

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 print("hellow world ")
 print("coba")
+print("hari : Selasa Tanggal: 21 November 2023 ")
+
+# git status
+# git add <nama file>
+# git commit -m "keterangan_commit"
+# git pull origin main
+# git push origin main
